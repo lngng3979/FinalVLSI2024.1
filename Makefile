f275adb91ef4807f7fa331f05e32f7c098f3a107
lngng3979 <lanhnguyen2k3hy@gmail.com>
2024-12-22 21:50:32 +0700

    Temp

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
-tb_sfifo.exe
+tb_sfifo.exe : *.v
+	iverilog -g2005-sv *.v -o tb_sfifo.exe
+
+
 
--- tb_sfifo.v
+++ tb_sfifo.v
@@ -1,1 +1,24 @@
+`timescale 1ns/1ps
+`define PERIOD 10
 module tb_sfifo();
+	reg	 			clk , resetn ;
+	reg 	[ WIDTH -1 : 0] 	wdata ;
+	reg	 			i_wreq;
+    	wire 	                 	o_wready;
+    	//read
+    	reg [  WIDTH-1:0  ] 		rdata;
+    	reg	                   	i_rreq;
+    	wire                 		o_rready;
+    	//fifo status
+    	wire 				fifo_isfull;
+    	wire    			fifo_isempty;
+        
+	wire [ADDR_WIDTH -1: 0] 	wr_pointer;
+	wire [ADDR_WIDTH -1:0 ]   	rd_pointer;
+	wire [ADDR_WIDTH -1 :0] 	fifo_dcount;
+	
+
+
+
+
+;
